94e95ad1e3f9ea0a8895bd9d85d7b7d477f29ff7
Yaroslav <86099208+YaroslavRom@users.noreply.github.com>
2021-07-20 16:13:39 +0300

    Update burger_menu.js

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -2,7 +2,6 @@ $(document).ready(function (){
     $('.header_burger-menu').click(function (event){
         $('.header_menu').toggleClass('menu_opened');
 
-        // $('body').toggleClass('lock');
     })
 })
 
